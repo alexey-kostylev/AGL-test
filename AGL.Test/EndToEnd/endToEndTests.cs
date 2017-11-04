6311a8a1c3617f09a9d10512fdf6c1b5c8304713
Alexey Kostylev <alx.kost@gmail.com>
2017-11-04 14:23:58 +1300

    added end-to-end test

---
+++ AGL.Test/EndToEnd/endToEndTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using AGL.App.Unity;
+using AGL.App.Controllers;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace AGL.Test.EndToEnd
+{
+    [TestClass]
+    public class endToEndTests
+    {
+        private IUnityContainer _container = new UnityContainer();
+        [TestInitialize]
+        public void TestInit()
+        {
+            _container.WireupAglDependencies();
+        }
+
+        [TestMethod]
+        public async Task ShouldGetDataFromAzureAndMakeView()
+        {
+            var controller = _container.Resolve<PeopleControler>();
+            var view = await controller.GetCatsWithOwnersGender();
+            view.Should().NotBeNullOrEmpty();
+        }
+    }
+}
